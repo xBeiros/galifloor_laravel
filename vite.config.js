0e556232ab5d900966b6dbcb7ce095584528689d
Kazim Duran <49122692+KazimDuran@users.noreply.github.com>
2025-07-26 14:24:45 +0200

    sites with [] not be loaded

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import {defineConfig} from "vite";
 
 export default defineConfig({
     plugins: [
@@ -17,4 +18,9 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
+        },
+    },
 });
